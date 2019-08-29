868313f32ed57413d6fd376e3682ba87325a1d60
Jimskapt <rami.programmer@gmail.com>
2019-08-29 06:24:54 +0200

    :boom: Printing result in stdout instead of a file.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,11 @@ use std::io::prelude::*;
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
 
-	println!("Castor Whispers 1.0.0");
-
 	if args.len() < 1 {
-		panic!("Please specify path to your file");
+		panic!("Please specify the path to your input file");
 	}
 
 	let input_path = args[1].as_str();
-
-	let output_path = if args.len() > 2 {
-		args[2].as_str()
-	} else {
-		input_path
-	};
-
-	println!("Processing {} to {} ...", input_path, output_path);
 
 	let mut raw = std::fs::File::open(input_path)
 		.expect(&format!("Can not open file {}", input_path));
@@ -32,13 +22,13 @@ fn main() {
 
 	let mut new_content = String::new(); 
 
-	let mut previous = None;
+	let mut previous: Option<&str> = None;
 	let mut current_is_code = false;
 	for line in lines {
 		if !current_is_code {
 			match previous {
 				Some(text) => {
-					if text == "" {
+					if text.trim() == "" {
 						new_content.push_str("<!--\n");
 					}
 				},
@@ -62,6 +52,5 @@ fn main() {
 		previous = Some(line.trim());
 	}
 
-	std::fs::write(output_path, new_content.as_str())
-		.expect(&format!("Can not write in file {}", output_path));
+	println!("{}", new_content);
 }
