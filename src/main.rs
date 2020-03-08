b111ab9d178dce2d11aa88e20d5d9afc94a0bb16
Jimskapt <rami.programmer@gmail.com>
2020-03-08 20:00:41 +0100

    :boom: Using clap crate in order to refactoring the CLI API.

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,109 @@
-use std::io::prelude::*;
-
 fn main() {
-	let args: Vec<String> = std::env::args().collect();
+	let app = clap::App::new("Castor Whispers")
+				.version(env!("CARGO_PKG_VERSION"))
+				.about("An utility to work with markdown text files")
+				.subcommand(
+					clap::SubCommand::with_name("comment")
+						.about("Comment all paragraphs")
+						.arg(
+							clap::Arg::with_name("input")
+								.help("The input file which contains all paragraphs")
+								.required(true)
+						)
+				)
+				/*
+				.subcommand(
+					clap::SubCommand::with_name("uncomment")
+						.about("Get only uncommented paragraphs")
+						.arg(
+							clap::Arg::with_name("input")
+								.help("The input file which contains all paragraphs")
+								.required(true)
+						)
+				)
+				*/
+				/*
+				.subcommand(
+					clap::SubCommand::with_name("only-comment")
+						.about("Get only commented paragraphs")
+						.arg(
+							clap::Arg::with_name("input")
+								.help("The input file which contains all paragraphs")
+								.required(true)
+						)
+				)
+				*/;
+	let mut help_text = Vec::new();
+	app.write_help(&mut help_text).unwrap();
 
-	if args.len() < 1 {
-		panic!("Please specify the path to your input file");
-	}
+	let matches = app.get_matches();
 
-	let input_path = args[1].as_str();
+	let search_code_delemiter = regex::Regex::new(r"(`+``+)").unwrap();
 
-	let mut raw = std::fs::File::open(input_path)
-		.expect(&format!("Can not open file {}", input_path));
-	
-	let mut raws_buffer = String::new();
-	raw.read_to_string(&mut raws_buffer)
-		.expect(&format!("Can not read file {}", input_path));
-	
-	let raws = raws_buffer.as_str();
+	if let Some(subcommand) = matches.subcommand_matches("comment") {
+		let input_path = subcommand
+			.value_of("input")
+			.expect("Please supply input file path");
+		let input_content =
+			std::fs::read_to_string(input_path).expect("error while reading input file");
 
-	let lines: Vec<&str> = raws.split("\n").collect();
+		let lines: Vec<&str> = input_content.split("\n").collect();
 
-	let mut new_content = String::new(); 
+		let mut new_content = String::new();
 
-	let mut previous: Option<&str> = None;
-	let mut current_is_code = false;
-	for line in lines {
-		if !current_is_code {
-			match previous {
-				Some(text) => {
-					if text.trim() == "" {
+		let mut previous: Option<&str> = None;
+		let mut status: ParseStatus = ParseStatus::None;
+		for line in lines {
+			if status == ParseStatus::None {
+				match previous {
+					Some(text) => {
+						if text.trim() == "" {
+							new_content.push_str("<!--\n");
+						}
+					}
+					None => {
 						new_content.push_str("<!--\n");
 					}
-				},
-				None => {
-					new_content.push_str("<!--\n");
+				}
+
+				if line.trim() == "" {
+					new_content.push_str("-->\n");
 				}
 			}
 
-			if line.trim() == "" {
-				new_content.push_str("-->\n");
+			// TODO : find case when openning ``` and closing ``` on same line.
+			match search_code_delemiter.captures(line) {
+				Some(found) => {
+					let delimiter = found[1].to_owned();
+
+					match &status {
+						ParseStatus::Code(current_delimiter) => {
+							if delimiter == *current_delimiter {
+								status = ParseStatus::None;
+							}
+						}
+						ParseStatus::None => {
+							status = ParseStatus::Code(delimiter.clone());
+						}
+					}
+				}
+				None => {}
 			}
+
+			new_content.push_str(line.replace("-->", "-- >").as_str());
+			new_content.push_str("\n");
+
+			previous = Some(line.trim());
 		}
 
-		if line.contains("```") {
-			current_is_code = !current_is_code;
-		}
-
-		new_content.push_str(line.replace("-->", "-- >").as_str());
-		new_content.push_str("\n");
-
-		previous = Some(line.trim());
+		println!("{}", new_content);
+	} else {
+		println!("{}", std::str::from_utf8(&help_text).unwrap());
 	}
+}
 
-	println!("{}", new_content);
+#[derive(PartialEq)]
+enum ParseStatus {
+	None,
+	Code(String),
 }
